fb2e28318610a5d2f4525107d50c65bfce196a2e
Kertesz Karoly <karoly.kertesz11@gmail.com>
2021-07-22 18:48:00 +0200

    added actions,reducers,middlewares

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore,combineReducers,applyMiddleware} from "redux"
+import Thunk from "redux-thunk"
+import {Provider} from "react-redux"
+import {singleUser,usersReducer} from "./reducers/index"
+const store = createStore(combineReducers({
+  user: singleUser,
+  users: usersReducer
+}),applyMiddleware(Thunk))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
--- server/app.js
+++ server/app.js
@@ -3,13 +3,16 @@ const app = express();
 const cors = require("cors")
 const cookieParser = require("cookie-parser");
 const apiv1 =require("./api/v1")
+const path = require("path")
+const fronthPath = path.join(__dirname, "public",)
 
-
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 app.use(cors({origin: "http://localhost:3000"}))
 app.use("/v1",apiv1)
-
+app.get("/*",(req,res)=> {
+  res.sendFile(path.join(fronthPath ,"index.html"))
+})
 module.exports = app
 
 
